116b01392a0269d3c7e4c07e1ed037793c3a20ac
unknown <3fredclark3@gmail.com>
2021-04-15 13:57:59 -0600

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [quotes, setQuotes] = useState([{ quote: "", author: "" }]);
+  const [quote, setQuote] = useState({ quote: "", author: "" });
+
+  useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((data) => data.json())
+      .then(({ quotes }) => {
+        setQuotes(quotes);
+        setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  function changeQuote() {
+    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="AppWrapper"
+      style={{
+        backgroundColor: "dodgerblue",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "0.5rem",
+      }}
+    >
+      <div
+        className="QuoteBox"
+        style={{
+          backgroundColor: "white",
+          padding: "1rem",
+          maxWidth: "500px",
+          margin: "auto",
+        }}
+      >
+        <div className="quotes-wrapper">
+          <span className="text">{quote.quote}</span>
+          <span className="author">{quote.author}</span>
+        </div>
+        <div className="bottom-quotebox">
+          <a target="_blank" href="https://twitter.com/" className="TweetQuote">
+            Tweet this Quote
+          </a>
+          <button className="newquote" onClick={changeQuote}>
+            New Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
